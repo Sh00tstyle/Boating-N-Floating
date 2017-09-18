ca5499220f323344be29d1a27b44454df04c7b4a
Leon Koster <leon-koster@web.de>
2017-09-18 23:13:40 +0200

    AI Tweaking and minor AI bugfixes for Tuesday

--- Assets/Scripts/AI/GoalEliminateThreat.cs
+++ Assets/Scripts/AI/GoalEliminateThreat.cs
@@ -36,63 +36,69 @@ public class GoalEliminateThreat : Goal {
         Quaternion targetRotation = Quaternion.LookRotation(targetDistance);
         targetRotation *= Quaternion.Euler(0, 90, 0); // this adds a 90 degrees Y rotation
 
-        if (targetDistance.magnitude < Behaviour.AttackRange - 1f) {
+        if (targetDistance.magnitude < Behaviour.AttackRange) {
             if (!Behaviour.Flocking.FireMode) {
                 Behaviour.Flocking.FireMode = true;
                 Behaviour.Flocking.Velocity = Vector3.zero;
             }
 
-            Ray rayLeft = new Ray(transform.position, -transform.right);
-            Ray rayRight = new Ray(transform.position, transform.right);
+            Ray rayLeft = new Ray(transform.position + new Vector3(0,1,0), -transform.right);
+            Ray rayRight = new Ray(transform.position + new Vector3(0, 1, 0), transform.right);
 
             RaycastHit hitLeft;
             RaycastHit hitRight;
 
-            Debug.DrawRay(transform.position, -transform.right * Behaviour.AttackRange, Color.cyan);
-            Debug.DrawRay(transform.position, transform.right * Behaviour.AttackRange, Color.cyan);
+            Debug.DrawRay(transform.position + new Vector3(0, 1, 0), -transform.right * Behaviour.AttackRange, Color.cyan);
+            Debug.DrawRay(transform.position + new Vector3(0, 1, 0), transform.right * Behaviour.AttackRange, Color.cyan);
 
 
             if (Physics.Raycast(rayLeft, out hitLeft, Behaviour.AttackRange)) {
                 if (fireCooldownTimer <= 0 && hitLeft.collider.tag == Tags.PLAYER) {
+                    transform.rotation = Quaternion.LookRotation(targetDistance) * Quaternion.Euler(0, Random.Range(80, 100), 0);
                     CannonballSpawnerEnemy[] cannons = cannonsLeft.GetComponentsInChildren<CannonballSpawnerEnemy>();
 
                     foreach (CannonballSpawnerEnemy c in cannons) {
                         c.Fire(Behaviour.Flocking.Velocity);
                     }
-                    fireCooldownTimer = Behaviour.FireCooldown;                 
+                    fireCooldownTimer = Behaviour.FireCooldown;
+                    return;              
                 }
-                else if (hitLeft.collider.tag == Tags.ENEMY) {
+                /*else if (hitLeft.collider.tag == Tags.ENEMY) {
                     //transform.position = Vector3.Slerp(transform.position, transform.position + (transform.forward.normalized * 5), Time.deltaTime * RotationSpeed);
-                }
+                }*/
             }
 
-            else if(Physics.Raycast(rayRight, out hitRight, Behaviour.AttackRange)) {
+            if(Physics.Raycast(rayRight, out hitRight, Behaviour.AttackRange)) {
                 if (fireCooldownTimer <= 0 && hitRight.collider.tag == Tags.PLAYER) {
+                    transform.rotation = Quaternion.LookRotation(targetDistance) * Quaternion.Euler(0, Random.Range(-80, -100), 0);
                     CannonballSpawnerEnemy[] cannons = cannonsRight.GetComponentsInChildren<CannonballSpawnerEnemy>();
 
                     foreach (CannonballSpawnerEnemy c in cannons) {
                         c.Fire(Behaviour.Flocking.Velocity);
                     }
                     fireCooldownTimer = Behaviour.FireCooldown;
+                    return;
                 }
-                else if(hitRight.collider.tag == Tags.ENEMY) {
+                /*else if(hitRight.collider.tag == Tags.ENEMY) {
                     //transform.position = Vector3.Slerp(transform.position, transform.position + (transform.forward.normalized * 5), Time.deltaTime * RotationSpeed);
-                }
+                }*/
             }
 
-            else {
-
+            if (fireCooldownTimer <= 0) {
                 Vector3 distanceLeftPlayer = Behaviour.Player.position - distanceCheckPointLeft.position;
                 Vector3 distanceRightPlayer = Behaviour.Player.position - distanceCheckPointRight.position;
 
-                if(distanceLeftPlayer.magnitude < distanceRightPlayer.magnitude) {
-                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 45, 0) * transform.rotation, Time.deltaTime * RotationSpeed);
+                //Turn left
+                if (distanceLeftPlayer.magnitude < distanceRightPlayer.magnitude) {
+                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 15, 0) * transform.rotation, Time.deltaTime * RotationSpeed);
                 }
-                else if(distanceLeftPlayer.magnitude > distanceRightPlayer.magnitude) {
-                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, -45, 0) * transform.rotation, Time.deltaTime * RotationSpeed);
+                //Turn right
+                else if (distanceLeftPlayer.magnitude > distanceRightPlayer.magnitude) {
+                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, -15, 0) * transform.rotation, Time.deltaTime * RotationSpeed);
                 }
+                //If both are the same lenght turn left
                 else {
-                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 45, 0) * transform.rotation, Time.deltaTime * RotationSpeed);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 15, 0) * transform.rotation, Time.deltaTime * RotationSpeed);
                 }
                 //transform.position = Vector3.Slerp(transform.position, Behaviour.Player.position - (targetDistance.normalized * (Behaviour.AttackRange * 0.85F)), Time.deltaTime * RotationSpeed);
             }
---
+++ Assets/Scripts/ScreenShakeScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenShakeScript: MonoBehaviour {
+
+    private float _shakeY;
+    private float _shakeYSpeed;
+
+    private float _shakeX;
+    private float _shakeXSpeed;
+
+    private void Start() {
+        _shakeY = 0f;
+        _shakeYSpeed = 0.7f;
+
+        _shakeX = 0f;
+        _shakeXSpeed = 0.7f;
+    }
+
+    private void Update() {
+        Vector2 _newPosition = new Vector2(_shakeX, _shakeY);
+        if (_shakeY < 0) {
+            _shakeY *= _shakeYSpeed;
+        }
+        _shakeY = -_shakeY;
+
+        if(_shakeX < 0) {
+            _shakeX *= _shakeXSpeed;
+        }
+        _shakeX = -_shakeX;
+
+        transform.Translate(_newPosition, Space.Self);
+    }
+
+
+    public void ApplyScreenShake() {
+        _shakeX = 1f;
+        _shakeY = 1f;
+
+        _shakeXSpeed = 0.7f;
+        _shakeYSpeed = 0.7f;
+    }
+
+    public void ApplyScreenShake(float shakeX, float shakeY, float shakeSpeedX, float shakeSpeedY) {
+        _shakeX = shakeX;
+        _shakeY = shakeY;
+
+        _shakeXSpeed = shakeSpeedX;
+        _shakeYSpeed = shakeSpeedY;
+    }
+}
